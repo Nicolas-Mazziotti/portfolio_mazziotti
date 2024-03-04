1709a84e8f256ab4127b3da14b734eb8bf15a510
nicolas_mazziotti <nicolas.mazziotti1@gmail.com>
2024-03-04 15:57:13 -0300

    Modify Header and Navbar - Component and style

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,23 @@
 import './Header.css'
 
+import { ReactTyped } from 'react-typed'
+
 const Header = () => {
   return (
     <div className="header-container">
-        <h1>NICOLAS MAZZIOTTI</h1>
+       <div className='header-text-container'>
+        <h1>Nicolas Mazziotti</h1>
+        <div className='header-subtitle-container'>
+        <p>I am a&nbsp;</p>
+        <ReactTyped        
+            strings = {['frontend web developer', 'freelancer']}
+            typeSpeed= {80}
+            backSpeed={50} 
+            style = {{color: '#0B9C95'}}           
+            loop >
+        </ReactTyped>
+        </div>                           
+        </div> 
     </div>
   )
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,33 +1,33 @@
-import {UserIcon} from '@heroicons/react/24/outline'
-import {CheckCircleIcon} from '@heroicons/react/24/outline'
-import {BookOpenIcon} from '@heroicons/react/24/outline'
-import {EnvelopeIcon} from '@heroicons/react/24/outline'
-
+import { UserIcon, CheckCircleIcon, BookOpenIcon, EnvelopeIcon } from '@heroicons/react/24/outline'
 import './Navbar.css'
 
 const Navbar = () => {
   return (
     <div className='navbar-container'>
-      <ul>
+           <ul>
         <li>
           <UserIcon className="navbar-icons"/>
-          <span>About</span>
+          <span className='hidden-span'>About</span>         
         </li>
         <li>
           <CheckCircleIcon className="navbar-icons"/>
-          <span>Skills</span>
+          <span className='hidden-span'>Skills</span>
         </li>
         <li>
         <BookOpenIcon className="navbar-icons"/>
-          <span>Portfolio</span>
+          <span className='hidden-span'>Portfolio</span>
         </li>
         <li>
         <EnvelopeIcon className="navbar-icons"/>
-          <span>Contact</span>
+          <span className='hidden-span'>Contact</span>
         </li>
       </ul>
-    </div>
+ </div>
+      
+
+   
   )
 }
 
 export default Navbar
+
