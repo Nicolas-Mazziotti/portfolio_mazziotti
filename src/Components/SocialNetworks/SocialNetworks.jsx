bb42b0f17d7d0ff3cb987dffa70563b33f521469
nicolas_mazziotti <nicolas.mazziotti1@gmail.com>
2024-03-05 17:03:08 -0300

    Styles Header and Navbar

--- src/Components/SocialNetworks/SocialNetworks.jsx
+++ src/Components/SocialNetworks/SocialNetworks.jsx
@@ -1,12 +1,13 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faGithub, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons"
+import './SocialNetworks.css'
 
 const SocialNetworks = () => {
   return (
     <>
-    <FontAwesomeIcon icon={faGithub}/>
-    <FontAwesomeIcon icon={faLinkedin} />
-    <FontAwesomeIcon icon={faInstagram} />
+    <a href="https://github.com/Nicolas-Mazziotti"><FontAwesomeIcon icon={faGithub}/></a>
+    <a href="https://linkedin.com/in/nicolas-mazziotti"><FontAwesomeIcon icon={faLinkedin} /></a>
+    <a href="https://www.instagram.com/solvantech_"><FontAwesomeIcon icon={faInstagram} /></a>
     </>
   )
 }
