a98aa9a928302a72f860deb5b859d3dd4659c183
nicolas_mazziotti <nicolas.mazziotti1@gmail.com>
2024-03-07 10:36:07 -0300

    Configuro gh-pages para publicar y estilos Header + navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,30 +1,39 @@
-import { UserIcon, CheckCircleIcon, BookOpenIcon, EnvelopeIcon, Bars3Icon } from '@heroicons/react/24/outline'
+import { UserIcon, CheckCircleIcon, BookOpenIcon, EnvelopeIcon, Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import './Navbar.css'
+import { useState } from 'react'
 
 const Navbar = () => {
+  let [isOpen, setIsOpen] = useState(false)
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <>
-    <div className='container-menu-icon'>
-      <Bars3Icon/>
+    <div onClick={toggleMenu}className='container-menu-icon'>
+      {
+        isOpen ? <XMarkIcon/> :  <Bars3Icon/> 
+      }
+      
+      
       </div> 
-    <div className='navbar-container'>         
+      <div className={`navbar-container ${isOpen ? 'open' : ''}`}>             
            <ul>
-        <li>
-          <UserIcon className="navbar-icons"/>
-          <span className='hidden-span'>About</span>         
-        </li>
-        <li>
-          <CheckCircleIcon className="navbar-icons"/>
-          <span className='hidden-span'>Skills</span>
-        </li>
-        <li>
-        <BookOpenIcon className="navbar-icons"/>
-          <span className='hidden-span'>Portfolio</span>
-        </li>
-        <li>
-        <EnvelopeIcon className="navbar-icons"/>
-          <span className='hidden-span'>Contact</span>
-        </li>
+              <li>
+                <UserIcon className="navbar-icons"/>
+                <span className={'hidden-span'}>About</span>         
+              </li>
+              <li>
+                <CheckCircleIcon className="navbar-icons"/>
+                <span className='hidden-span'>Skills</span>
+              </li>
+              <li>
+              <BookOpenIcon className="navbar-icons"/>
+                <span className='hidden-span'>Portfolio</span>
+              </li>
+              <li>
+              <EnvelopeIcon className="navbar-icons"/>
+                <span className='hidden-span'>Contact</span>
+              </li>
       </ul>
  </div>
  </>
