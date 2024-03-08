4934d994d0e806983caa2724e8d208acf5ce157a
nicolas_mazziotti <nicolas.mazziotti1@gmail.com>
2024-03-08 10:58:55 -0300

    Add components and spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,45 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 //Components
 import Navbar from "./Components/Navbar/Navbar"
 import Header from "./Components/Header/Header"
+import About from "./Components/About/About";
+import Spinner from "./Components/Spinner/Spinner";
+
+import './App.css'
 
 
 function App() {
-
+  const [loading, setLoading] = useState(true)
   const [menuOpen, setMenuOpen] = useState(false)
+
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
+
+  useEffect(() => {
+    // Simular tiempo de carga
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000); // 3 segundos de carga simulada
+
+    // Limpiar el temporizador cuando se desmonte el componente
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <>
+    <div id='app'>
+        <>
+    {loading ? (<Spinner/>) : (
+      <>
       <Navbar toggleMenu={toggleMenu} menuOpen={menuOpen}/>
       <Header menuOpen={menuOpen}/>
+      <About/>            
+      </>
+      )}
     </>
-  )
+
+    </div>
+      )
 }
 
 export default App
---
+++ src/Components/Spinner/Spinner.jsx
@@ -0,0 +1,12 @@
+import './Spinner.css'
+
+const Spinner = () => {
+  return (
+    <div className='spinner-container'>
+      <span className="loader"></span>
+    </div>
+    
+  )
+}
+
+export default Spinner
