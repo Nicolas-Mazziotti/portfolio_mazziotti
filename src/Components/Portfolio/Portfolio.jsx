9d5905efcac30218c3f0d39205c67115aefc1465
nicolas_mazziotti <nicolas.mazziotti1@gmail.com>
2024-03-12 12:46:19 -0300

    Modify card buttons

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -2,39 +2,45 @@ import "./Portfolio.css";
 import portfolio_glitter from "/assets/portfolio_glitter.jpg"
 import portfolio_celulares from "/assets/portfolio_celulares.jpg"
 import portfolio_characters from "/assets/portfolio_characters.jpg"
+import portfolio_richard from "/assets/portfolio_richard.jpg"
 
 const Portfolio = () => {
   const cards = [
+    
     {
       id: 1,
-      title: "GlitterArt freelance",
-      subtitle: "HTML | CSS | BOOTSTRAP | JS",
-      url: "https://glitterart.es/",
-      img: portfolio_glitter,
-      alt: "glitterart",
-    },
-    {
-      id: 2,
       title: "XiCelulares ecommerce",
       subtitle: "REACT | FIREBASE",
       url: "https://nicolas-mazziotti.github.io/xicelulares-Mazziotti/",
       img: portfolio_celulares,
     },
     {
-      id: 3,
+      id: 2,
       title: "Rick and Morty Characters",
       subtitle: "HTML | CSS | JS",
       url: "https://nicolas-mazziotti.github.io/getApi/",
       img: portfolio_characters,
     },
+    {
+        id: 3,
+        title: "Crud Grill quoter",
+        subtitle: "HTML | CSS | JS | PHP | MYSQL",
+        url: "https://nicolas-mazziotti.github.io/getApi/",
+        img: portfolio_richard,
+      },
+      {
+        id: 4,
+        title: "GlitterArt freelance",
+        subtitle: "HTML | CSS | BOOTSTRAP | JS",
+        url: "https://glitterart.es/",
+        img: portfolio_glitter,
+        alt: "glitterart",
+      },
   ];
   return (
     <div className="portfolio-container">
       <div
-        className="portfolio-title-container"
-        data-aos="fade-up"
-        data-aos-duration="1000"
-      >
+        className="portfolio-title-container" data-aos="fade-up" data-aos-duration="1000">
         <h1>PORTFOLIO</h1>
         <div data-aos="fade-up" data-aos-duration="1000"></div>
       </div>
@@ -50,7 +56,9 @@ const Portfolio = () => {
                 <div className="card-content">
                   <h2 className="card-title">{card.title}</h2>
                   <p>{card.subtitle}</p>
-                  <a href="">Live demo</a>
+                  <div className="card-content-buttons">                    
+                    <a href="">Live demo</a>
+                  </div>
                 </div>
               </div>
             )
